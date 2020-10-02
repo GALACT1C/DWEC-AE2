66704e95a58ed82c3213dd895cbf43799dfd0bc0
Carlos <carlostamarit18@gmail.com>
2020-10-02 13:11:37 +0200

    Subo ejercicio 1 (falta rectificarlo)

---
+++ AE2-EJ01.js
@@ -0,0 +1,21 @@
+const array = [
+    {id: 1, valor: 'Ana'},
+    {id: 2, valor: 2},
+    {id: 3, valor: 'Javi'},
+    {id: 4, valor: 5},
+    {id: 5, valor: 'Roberto'},
+    {id: 6, valor: 7},
+    {id: 7, valor: 'Vanessa'},
+    {id: 8, valor: 10},
+    ];
+ 
+
+const valorArray = array.map(function(arry) {
+    if(typeof(arry) !='string'){
+        arry[id].
+    }
+    return arry.valor;
+});
+
+ 
+console.log(momentoComida);
