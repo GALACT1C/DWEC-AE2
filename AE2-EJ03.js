91252cfe57ced4c498baa243bc489bb267b6cec9
Carlos <carlostamarit18@gmail.com>
2020-10-02 12:59:12 +0200

    Subo ejercicio 3

---
+++ AE2-EJ03.js
@@ -0,0 +1,8 @@
+function findMinimum(values){
+    var minValue = Math.min(values);
+    return minValue;
+}
+
+var values=[2,5,6,7,8,9,4,5,6,7,8,1,7,8,6,0,5,4,9,5,3];
+
+console.log(findMinimum(values));
